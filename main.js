b975e2988ecf7ed99af00057d53f663dcdc3b21b
Nathan Ravenscraft <nraven22@gmail.com>
2020-09-21 08:32:17 -0400

    first commit, sytling needs work

---
+++ main.js
@@ -0,0 +1,30 @@
+//let total = 0
+console.log("javascript loaded")
+let text = ""
+let display = document.getElementById('display')
+console.log(display.id)
+
+for (let element of document.getElementsByClassName('btn')) {
+    element.addEventListener('click', function() {
+        console.log(element.innerText)
+        if (element.id === 'clear') {
+            text = ""
+            display.innerText = text
+            console.log("clear")
+        }
+        else if (element.id === 'eval') {
+            console.log("eval")
+            text = eval(text)
+            display.innerText = text
+        }
+        else {
+            console.log("click")
+            text += element.innerText
+            display.innerText = text 
+        }
+        
+        
+    })
+   
+
+}
